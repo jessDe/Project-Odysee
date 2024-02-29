04820d07adb384220326b675f492a209deb40df8
jessDe <colpix12@gmail.com>
2024-02-29 14:49:42 +0100

    Main Menu Animation and stuff

--- src/js/GameManager.js
+++ src/js/GameManager.js
@@ -8,26 +8,112 @@ MenuButton.src = "src/img/MenuButton.png";
 let Title = new Image();
 Title.src = "src/img/Title.png";
 
+
+let GateImage = new Image();
+GateImage.src = "src/img/Gate.png";
+
+
+let EyeImage = new Image();
+EyeImage.src = "src/img/EyeCentered.png";
+
+let EyeImage2 = new Image();
+EyeImage2.src = "src/img/eye.png";
+
+let ArrowImage = new Image();
+ArrowImage.src = "src/img/Arrow.png";
+
+
+/*
+    GameMode:
+    0 = MainMenu
+    1 = JumpAndRun
+    2 = TowerDefence
+
+    MainMenuMode:
+    0 = MainMenu
+    1 = Tutorial
+    2 = LevelSelect
+ */
+
 class MainMenu{
     constructor(){
+        this.RegisterEvents();
+        this.MainMenuMode = 0;
+        this.Transition = 0;
+        this.frame = 0;
     }
     Draw(){
-        ctx.drawImage(Title, 0,0, 634, 221, ctx.canvas.width/2- 400, ctx.canvas.height*0.1, 800, 300);
-        ctx.drawImage(MenuButton, 0,0,64,64, ctx.canvas.width/2- 100, ctx.canvas.height*0.7, 200,200);
+        this.frame -= 0.175;
+        if(this.MainMenuMode === 0){
+            ctx.drawImage(Title, 0,0, 634, 221, ctx.canvas.width/2- 400, ctx.canvas.height*0.1, 800, 300);
+            ctx.drawImage(MenuButton, 0,0,64,64, ctx.canvas.width/2- 100, ctx.canvas.height*0.7, 200,200);
+        }else if(this.MainMenuMode === 2){
+
+
+            //Rotate Outer Eye
+            let imageX = ctx.canvas.width/2-15 - 250;
+            let imageY = ctx.canvas.height/2-10 -250;
+            ctx.translate(imageX + 250, imageY + 250);
+            ctx.rotate(this.frame * Math.PI / 180);
+            ctx.drawImage(EyeImage, -250, -250, 500, 500);
+            ctx.rotate(-this.frame * Math.PI / 180);
+            ctx.translate(-(imageX + 250), -(imageY + 250));
+
+            //Rotate Inner Eye
+            let imageX2 = ctx.canvas.width/2 - 150;
+            let imageY2 = ctx.canvas.height/2 -172;
+            ctx.translate(imageX2 + 130, imageY2 + 130);
+            ctx.rotate(this.frame * Math.PI / 180);
+            ctx.drawImage(EyeImage, -130, -130, 260, 260);
+            ctx.rotate(-this.frame * Math.PI / 180);
+            ctx.translate(-(imageX2 + 130), -(imageY2 + 130));
+
+
+            //Removes Overflow of Gate
+            ctx.clearRect(0,0,500,700);
+            ctx.clearRect(0,0,1280,150);
+            ctx.clearRect(725,0,1280,800);
+            ctx.clearRect(0,600,1280,150);
+
+
+            //Draws Gate
+            ctx.drawImage(EyeImage2, ctx.canvas.width/2 - 52,ctx.canvas.height/2 -75,70,70);
+            ctx.drawImage(GateImage, ctx.canvas.width/2 - 290,ctx.canvas.height/2 -300,580,600);
+
+        }
+
+    }
+
+    RegisterEvents(){
+        ctx.canvas.addEventListener("click", function(event){
+            if(MainMenuObj.MainMenuMode === 0){
+                if(MousePos.x >= 26 && MousePos.x <= 36){
+                    if(MousePos.y >= 25 && MousePos.y <= 35){
+                        MainMenuObj.MainMenuMode = 2;
+                    }
+                }
+            }
+
+        });
     }
 }
 let MainMenuObj = new MainMenu();
 window.onload = function(){
     console.log("Game started"+ GameMode);
     if(GameMode === 0){
+        document.body.style.background = "#212121";
         document.getElementById("GameBox").style.width = "960px";
         document.getElementById("GameBox").style.height = "540px";
         document.getElementById("Shop").style.display = "none";
+        document.getElementById("debugText").style.display = "none";
+        document.getElementById("myCanvas").style.background = "#000000"
         Update();
     }else if(GameMode === 1){
         JumpAndRun.StartGame();
 
     }else if(GameMode === 2){
+        document.body.style.backgroundImage = "url('src/img/bg_egypt01.png')";
+        document.getElementById("debugText").style.display = "block";
         TD.StartGame();
     }
 
@@ -41,6 +127,7 @@ window.onload = function(){
 function Update(){
     if(GameMode === 0){
         MainMenuObj.Draw();
+
     }else if(GameMode === 1){
 
     }else if(GameMode === 2){
