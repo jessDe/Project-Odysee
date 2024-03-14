7a018a4171888f5ffe877282242af6437ac95710
noisetalk <noisetalk@t-online.de>
2024-03-14 08:44:18 +0100

    Errors included! Have a nice day! 2024-03-14, nt

--- src/js/JumpAndRun/Game.js
+++ src/js/JumpAndRun/Game.js
@@ -5,20 +5,19 @@ let steuerung = {
     rechts: false,
     springen: false,
     slide: false,
-    angriff1: false,
-    angriff2: false,
+    angriff: false,
+    special: false,
     magic: false,
     pause: false
 };
 
-//load Images
 
 
 class JumpAndRunClass {
     constructor(level) {
         this.curlevel = level;
         this.lvlc = LEVELS[this.curlevel];
-        this.zuletzt = new Date().getTime();
+        // this.zuletzt = new Date().getTime();
         this.myPlayer = new Player( this.lvlc.map.spawn.player.x, this.lvlc.map.spawn.player.y, 64, 64, this.lvlc.map );
         this.bgimg = new Image();
         this.bgimg.src = this.lvlc.bgimg;
@@ -43,7 +42,7 @@ class JumpAndRunClass {
             height: 0
         };
         let offset = {
-            x: this.myPlayer.x/TILESIZE - (canvas.width/TILESIZE)/2,
+            x: this.myPlayer.pos.x/TILESIZE - (canvas.width/TILESIZE)/2,
             y: 0,
         };
         let PlayerPos = this.lvlc.map.spawn.player;
@@ -84,11 +83,11 @@ class JumpAndRunClass {
             case "ArrowUp":
                 steuerung.springen = (event.type === 'keydown'); break;
             case "Space":
-                steuerung.angriff1 = (event.type === 'keydown'); break;
+                steuerung.angriff = (event.type === 'keydown'); break;
             case "Shift":
-                steuerung.angriff2 = (event.type === 'keydown'); break;
+                steuerung.special = (event.type === 'keydown'); break;
             case "Control":
-                steuerung.magie = (event.type === 'keydown'); break;
+                steuerung.magic = (event.type === 'keydown'); break;
             case "Escape":
                 steuerung.pause = (event.type === 'keydown'); break;
             default:
@@ -97,19 +96,148 @@ class JumpAndRunClass {
         event.preventDefault();        // Cancel the default action to avoid it being handled twice
     }
 
+    juggler( ntt, sprite ) {
+        if (ntt.image === ntt.sprites.death.image) {
+            if (ntt.frame === ntt.sprites.death.frMax - 1)
+                ntt.alive = false;
+            return;
+        }
+        // Ausführen von Angriff1 cancelt die aktuelle Animation
+        if (
+            ntt.image === ntt.sprites.angriffL.image &&
+            ntt.frame < ntt.sprites.angriffL.frMax - 1
+        )
+            return;
+        // Einstecken eines Treffers cancelt die aktuelle Animation
+        if (
+            ntt.image === ntt.sprites.struck.image &&
+            ntt.frame < ntt.sprites.struck.frMax - 1
+        )
+            return;
+
+        // Switch-Verzweigung für die einzelnen States
+        switch (sprite) {
+            case 'idleL':
+                if (ntt.image !== ntt.sprites.idleL.image) {
+                    ntt.frMax = ntt.sprites.idleL.frMax;
+                    ntt.image = ntt.sprites.idleL.image;
+                }
+                break;
+            case 'idleR':
+                if (ntt.image !== ntt.sprites.idleR.image) {
+                    ntt.frMax = ntt.sprites.idleR.frMax;
+                    ntt.image = ntt.sprites.idleR.image;
+                }
+                break;
+            case 'runLeft':
+                if (ntt.image !== ntt.sprites.runLeft.image) {
+                    ntt.frMax = ntt.sprites.runLeft.frMax;
+                    ntt.image = ntt.sprites.runLeft.image;
+                }
+                break;
+            case 'runRight':
+                if (ntt.image !== ntt.sprites.runRight.image) {
+                    ntt.frMax = ntt.sprites.runRight.frMax;
+                    ntt.image = ntt.sprites.runRight.image;
+                }
+                break;
+            case 'jumpL':
+                if (ntt.image !== ntt.sprites.jumpL.image) {
+                    ntt.frMax = ntt.sprites.jumpL.frMax;
+                    ntt.image = ntt.sprites.jumpL.image;
+                }
+                break;
+            case 'jumpR':
+                if (ntt.image !== ntt.sprites.jumpR.image) {
+                    ntt.frMax = ntt.sprites.jumpR.frMax;
+                    ntt.image = ntt.sprites.jumpR.image;
+                }
+                break;
+            case 'slideL':
+                if (ntt.image !== ntt.sprites.slideL.image) {
+                    ntt.frMax = ntt.sprites.slideL.frMax;
+                    ntt.image = ntt.sprites.slideL.image;
+                }
+                break;
+            case 'slideR':
+                if (ntt.image !== ntt.sprites.slideR.image) {
+                    ntt.frMax = ntt.sprites.slideR.frMax;
+                    ntt.image = ntt.sprites.slideR.image;
+                }
+                break;
+            case 'attackL':
+                if (ntt.image !== ntt.sprites.angriffL.image) {
+                    ntt.frMax = ntt.sprites.angriffL.frMax;
+                    ntt.image = ntt.sprites.angriffL.image;
+                }
+                break;
+            case 'attackR':
+                if (ntt.image !== ntt.sprites.angriffR.image) {
+                    ntt.frMax = ntt.sprites.angriffR.frMax;
+                    ntt.image = ntt.sprites.angriffR.image;
+                }
+                break;
+            case 'magicL':
+                if (ntt.image !== ntt.sprites.magicL.image) {
+                    ntt.frMax = ntt.sprites.magicL.frMax;
+                    ntt.image = ntt.sprites.magicL.image;
+                }
+                break;
+            case 'magicR':
+                if (ntt.image !== ntt.sprites.magicR.image) {
+                    ntt.frMax = ntt.sprites.magicR.frMax;
+                    ntt.image = ntt.sprites.magicR.image;
+                }
+                break;
+            case 'struck':
+                if (ntt.image !== ntt.sprites.struck.image) {
+                    ntt.frMax = ntt.sprites.struck.frMax;
+                    ntt.image = ntt.sprites.struck.image;
+                }
+                break;
+            case 'death':
+                if (ntt.image !== ntt.sprites.death.image) {
+                    ntt.frMax = ntt.sprites.death.frMax;
+                    ntt.image = ntt.sprites.death.image;
+                }
+                break;
+        }
+    }
+
+
+    // Methode zum Prüfen ob Einheiten getroffen wurden
+    struck( attacker, target ) {
+        if(!target.invulnerable){
+            target.hitpoints -= attacker.atkPow; // Variable für Gegnerangriff einbauen!!!
+            if (target.hitpoints <= 0) {
+                target.juggler('death');
+            } else target.juggler('struck');
+        }
+    }
+
     Unload(){
         window.removeEventListener('keydown', this.steuern);
         window.removeEventListener('keyup', this.steuern);
     }
 
     updateGame() {
+        /* // redudant
         let jetzt = new Date().getTime();
         let dauer = ( jetzt - JumpAndRun.zuletzt ) /1000 ;
-        JumpAndRun.drawLevel();
         JumpAndRun.zuletzt = jetzt;
+        */
+        JumpAndRun.drawLevel();
         JumpAndRun.myPlayer.update();
         if(JumpAndRun.GameRunning){
             window.requestAnimationFrame( JumpAndRun.updateGame );
         }
     }
+}
+
+// Kollision von zwei Rechtecken
+function rectCollision( rect1, rect2 ) {
+    return( rect1.pos.x < rect2.pos.x + rect2.size.w &&
+            rect1.pos.x + rect1.size.w > rect2.pos.x &&
+            rect1.pos.y < rect2.pos.y + rect2.size.h &&
+            rect1.pos.y + rect1.size.h > rect2.pos.y );
 }
--- src/js/JumpAndRun/Player.js
+++ src/js/JumpAndRun/Player.js
@@ -3,13 +3,25 @@
 class Player {
   constructor(x, y, w, h, map) {
     // Position und Größe des Spielers
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
+    this.pos = {
+      x: x,
+      y: y
+    };
+    this.size = {
+      w: w,
+      h: h
+    };
+    this.offset = {
+      x: 0,
+      y: 0
+    };
+    this.eastward = true;
     // Bewegungsgeschwindigkeit, Sprungkraft, Schwerkraft und Air-Stair
     this.speed = 5;
-    this.velocitY = 1;
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
     this.jumpStrength = 15;
     this.gravity = 0.981;
     this.airStair = 0;
@@ -23,18 +35,37 @@ class Player {
     this.mag2nd = 20;
     this.atkCooldown = 250;
     this.hasDeflect = false;
-    this.offensive = false;
-
-    this.offsetX = 0;
-    this.offsetY = 0;
+    this.attacking = false;
+    this.slideCooldown = 0;
+    this.slideDuration = 0;
+    this.isSliding = false;
+    this.invulnerable = false;
+    this.atkBox = {
+      pos: {
+        x: x,
+        y: y
+      },
+      size: {
+        w: 48,
+        h: 48
+      },
+      offset: {
+        x: 0,
+        y: 0
+      }
+    }
     // Animation und Darstellung
     this.frame = 0;
     this.frPast = 0;
     this.frMax = 12;
     this.image = new Image();
-    this.image.src = './src/img/pc/runRight.png';
+    this.image.src = './src/img/pc/idleR.png';
     this.sprites = {
-      idle: {
+      idleL: {
+        image: new Image(),
+        frMax: 12
+      },
+      idleR: {
         image: new Image(),
         frMax: 12
       },
@@ -46,23 +77,43 @@ class Player {
         image: new Image(),
         frMax: 12
       },
-      jump: {
+      jumpL: {
         image: new Image(),
         frMax: 12
       },
-      slide: {
+      jumpR: {
+        image: new Image(),
+        frMax: 12
+      },
+      slideL: {
+        image: new Image(),
+        frMax: 12
+      },
+      slideR: {
+        image: new Image(),
+        frMax: 12
+      },
+      angriffL: {
         image: new Image(),
         frMax: 1
       },
-      angriff1: {
+      angriffR: {
         image: new Image(),
         frMax: 1
       },
-      angriff2: {
+      specialL: {
         image: new Image(),
         frMax: 1
       },
-      magic: {
+      specialR: {
+        image: new Image(),
+        frMax: 1
+      },
+      magicL: {
+        image: new Image(),
+        frMax: 1
+      },
+      magicR: {
         image: new Image(),
         frMax: 1
       },
@@ -76,27 +127,44 @@ class Player {
       }
     }
 
-    this.sprites.idle.image.src = './src/img/pc/runRight.png';
+    this.sprites.idleL.image.src = './src/img/pc/idleL.png';
+    this.sprites.idleR.image.src = './src/img/pc/idleR.png';
     this.sprites.runLeft.image.src = './src/img/pc/runLeft.png';
     this.sprites.runRight.image.src = './src/img/pc/runRight.png';
-    this.sprites.jump.image.src = './src/img/pc/runRight.png';
-    this.sprites.slide.image.src = './src/img/pc/runRight.png';
+    this.sprites.jumpL.image.src = './src/img/pc/jumpL.png';
+    this.sprites.jumpR.image.src = './src/img/pc/jumpR.png';
+    this.sprites.slideL.image.src = './src/img/pc/slideL.png';
+    this.sprites.slideR.image.src = './src/img/pc/slideR.png';
+    // this.sprites.angriffL.image.src = './src/img/pc/angriffL.png';
+    // this.sprites.angriffR.image.src = './src/img/pc/angriffR.png';
+    // this.sprites.specialL.image.src = './src/img/pc/specialL.png';
+    // this.sprites.specialR.image.src = './src/img/pc/specialR.png';
+    // this.sprites.magicL.image.src = './src/img/pc/magicL.png';
+    // this.sprites.magicR.image.src = './src/img/pc/magicR.png';
+    // this.sprites.struck.image.src = './src/img/pc/struck.png';
+    // this.sprites.death.image.src = './src/img/pc/death.png';
 
     // Sonstiges
     this.map = map;
   }
+
+  //
+
+
+
   // Kollisionsmethode für Terrain, übernommen vom J&R aus dem Unterricht
   blockade( pixelX, pixelY, map ) {
     let zeichenLO, zeichenLU, zeichenRO, zeichenRU;
     let b = {} ;
     b.spalteLinks = Math.floor( pixelX / TILESIZE ) ;
-    b.spalteRechts = Math.floor( ( pixelX + this.w ) / TILESIZE ) ;
+    b.spalteRechts = Math.floor( ( pixelX + this.size.w ) / TILESIZE ) ;
     b.zeileOben = Math.floor( pixelY / TILESIZE ) ;
-    b.zeileUnten = Math.floor( ( pixelY + this.h ) / TILESIZE ) ;
+    b.zeileUnten = Math.floor( ( pixelY + this.size.h ) / TILESIZE ) ;
     zeichenLO = map.pattern[ b.zeileOben ].charAt( b.spalteLinks ) ;
     zeichenLU = map.pattern[ b.zeileUnten ].charAt( b.spalteLinks ) ;
     zeichenRO = map.pattern[ b.zeileOben ].charAt( b.spalteRechts ) ;
     zeichenRU = map.pattern[ b.zeileUnten ].charAt( b.spalteRechts ) ;
+    // console.log(zeichenLU + " " +zeichenRU)
     b.links = ( map.solid.indexOf( zeichenLO ) >= 0 || map.solid.indexOf( zeichenLU ) >= 0 ) ;
     b.rechts = ( map.solid.indexOf( zeichenRO ) >= 0 || map.solid.indexOf( zeichenRU ) >= 0 ) ;
     b.oben = ( map.solid.indexOf( zeichenLO ) >= 0 || map.solid.indexOf( zeichenRO ) >= 0 ) ;
@@ -104,168 +172,112 @@ class Player {
     return b;
   }
 
-  // Methode zum Handlen von Treffern am PC
-  struck() {
-    this.hitpoints -= 10; // Variable für Gegnerangriff einbauen!!!
-    if (this.hitpoints <= 0) {
-      this.juggler('death');
-    } else this.juggler('struck');
-  }
-
-
-  // Puppeteer: PC-Bewegung
+  // Puppeteer: PC-Bewegung und Animationen
   puppeteer(){
-        // Idle
-        // if (!steuerung) {
-        //  this.juggler('idle');
-        // }
+    this.velocity.x = 0;
+    // PC in Ruheposition
+    if (!steuerung.links && !steuerung.rechts && this.airStair === 0 && !steuerung.slide && !steuerung.angriff && !steuerung.special && !steuerung.magic) {
+      if( this.eastward ) JumpAndRun.juggler( JumpAndRun.myPlayer,'idleR');
+      else JumpAndRun.juggler( JumpAndRun.myPlayer,'idleL');
+    }
     // PC nach links bewegen
     if (steuerung.links) {
-      this.x -= this.speed;
-      let blockiert = this.blockade( this.x, this.y, this.map ) ;
-      if( blockiert.links ) this.x = TILESIZE * blockiert.spalteLinks + TILESIZE;
-      this.juggler('runLeft');
+      this.pos.x -= this.speed;
+      this.eastward = false;
+      JumpAndRun.juggler( JumpAndRun.myPlayer, 'runLeft');
+      let blockiert = this.blockade( this.pos.x, this.pos.y, this.map ) ;
+      if( blockiert.links ) this.pos.x = TILESIZE * blockiert.spalteLinks + TILESIZE;
     }
     // PC nach rechts bewegen
     if (steuerung.rechts) {
-      this.x += this.speed;
-      let blockiert = this.blockade( this.x, this.y, this.map ) ;
-      if( blockiert.rechts ) this.x = TILESIZE * blockiert.spalteRechts - this.w - 1 ;
-      this.juggler('runRight');
+      this.pos.x += this.speed;
+      this.eastward = true;
+      if( this.airStair === 0) JumpAndRun.juggler( JumpAndRun.myPlayer, 'runRight');
+      let blockiert = this.blockade( this.pos.x, this.pos.y, this.map ) ;
+      if( blockiert.rechts ) this.pos.x = TILESIZE * blockiert.spalteRechts - this.size.w - 1 ;
     }
     // PC springen lassen + Multi-Jump-Funktionalität ("Air-Stair")
     if (steuerung.springen && this.airStair < this.airStairLimit) {
       steuerung.springen = false;
-      this.velocitY = this.jumpStrength * -1;
+      this.velocity.y = this.jumpStrength * -1;
       this.airStair++;
-      // console.log("AS: "+ this.airStair +" - ASL: "+ this.airStairLimit +" - steuerung.springen: "+ steuerung.springen);
-      this.juggler('jump');
+      if( this.eastward ) JumpAndRun.juggler( JumpAndRun.myPlayer, 'jumpR');
+      else JumpAndRun.juggler( JumpAndRun.myPlayer, 'jumpL');
     }
-    // PC rutschen lassen
-    if (steuerung.slide) {
-      this.juggler('slide');
+    //
+    if (steuerung.slide && this.slideCooldown === 0) {
+      this.isSliding = true;
+      this.invulnerable = true;
+      this.slideCooldown = 100;
+      this.slideDuration = 15;
+      if( this.eastward ) JumpAndRun.juggler( JumpAndRun.myPlayer, 'slideR');
+      else JumpAndRun.juggler( JumpAndRun.myPlayer, 'slideL');
     }
     // PC angreifen lassen
-    if (steuerung.angriff1) {
-      this.juggler('angriff1');
-      this.offensive = true;
+    if (steuerung.angriff) {
+      if( this.eastward ) JumpAndRun.juggler( JumpAndRun.myPlayer, 'attackR');
+      else JumpAndRun.juggler( JumpAndRun.myPlayer, 'attackL');
+      this.attacking = true;
     }
     // PC angreifen lassen
-    if (steuerung.angriff2) {
-
+    if (steuerung.special) {
+      if( this.eastward ) JumpAndRun.juggler( JumpAndRun.myPlayer, 'specialR');
+      else JumpAndRun.juggler( JumpAndRun.myPlayer, 'specialL');
     }
     // Spieler Magie einsetzen lassen
     if (steuerung.magic) {
-      // NYI
+      if( this.eastward ) JumpAndRun.juggler( JumpAndRun.myPlayer, 'magicR');
+      else JumpAndRun.juggler( JumpAndRun.myPlayer, 'magicL');
     }
     // Spiel pausieren
     if (steuerung.pause) {
       // Spiel pausieren
 
     }
+
+    if(this.slideDuration > 0){
+      if(this.eastward){
+        this.velocity.x = this.slideDuration * 2;
+      }else{
+        this.velocity.x = -this.slideDuration * 2;
+      }
+
+
+      // console.log("Sliding")
+    }
     // PC Fallkontrolle
-    this.velocitY += this.gravity;
-    if (this.y < this.map.height * TILESIZE - this.h) this.y += this.velocitY;
-    let blockiert = this.blockade( this.x, this.y, this.map ) ;
-    if (this.velocitY > 0 && blockiert.unten ) {
-      this.y = TILESIZE * blockiert.zeileUnten - this.h - 0.1;
-      this.velocitY = 0;
+    this.velocity.y += this.gravity;
+    if (this.pos.y < this.map.height * TILESIZE - this.size.h) this.pos.y += this.velocity.y;
+    let blockiert = this.blockade(Math.floor(this.pos.x), Math.floor(this.pos.y), this.map ) ;
+    if (this.velocity.y > 0 && blockiert.unten ) {
+      this.pos.y = TILESIZE * blockiert.zeileUnten - this.size.h - 2;
+      this.velocity.y = 0;
       this.airStair = 0;
     }
-    if (this.velocitY < 0 && blockiert.oben ) {
-      this.y = TILESIZE * blockiert.zeileOben + TILESIZE;
-      this.velocitY = 0;
+    if (this.velocity.y < 0 && blockiert.oben ) {
+      this.pos.y = TILESIZE * blockiert.zeileOben + TILESIZE;
+      this.velocity.y = 0;
     }
-  }
+    if(blockiert.links || blockiert.rechts){
+      this.velocity.x = 0;
+      // console.log("blocked sliding"+ blockiert.links + " "+blockiert.rechts)
+    }else{
+      // Verhindert dass der PC mit Slide
+      if(this.blockade(Math.floor(this.pos.x)+TILESIZE, Math.floor(this.pos.y), this.map).rechts && this.eastward){
+        this.velocity.x = 0;
+      }else if(this.blockade(Math.floor(this.pos.x)-TILESIZE, Math.floor(this.pos.y), this.map).links && this.eastward === false){
+        this.velocity.x = 0;
+      }else{
+        this.pos.x += this.velocity.x;
+      }
 
-  // Juggler: "Jongliert" mit den Sprites des PCs
-  // geschrieben von: AZ
-  juggler( sprite ) {
-    if (this.image === this.sprites.death.image) {
-      if (this.frame === this.sprites.death.frMax - 1)
-        this.alive = false;
-      return;
     }
-    // Ausführen von Angriff1 cancelt die aktuelle Animation
-    if (
-        this.image === this.sprites.angriff1.image &&
-        this.frame < this.sprites.angriff1.frMax - 1
-       )
-    return;
-    // Einstecken eines Treffers cancelt die aktuelle Animation
-    if (
-        this.image === this.sprites.struck.image &&
-        this.frame < this.sprites.struck.frMax - 1
-       )
-    return;
-    // Switch-Verzweigung für die einzelnen States
-    switch (sprite) {
-      case 'idle':
-        if (this.image !== this.sprites.idle.image) {
-          this.frMax = this.sprites.idle.frMax;
-          this.image = this.sprites.idle.image;
-        }
-        break;
-      case 'runLeft':
-        if (this.image !== this.sprites.runLeft.image) {
-          this.frMax = this.sprites.runLeft.frMax;
-          this.image = this.sprites.runLeft.image;
-        }
-        break;
-      case 'runRight':
-        if (this.image !== this.sprites.runRight.image) {
-          this.frMax = this.sprites.runRight.frMax;
-          this.image = this.sprites.runRight.image;
-        }
-        break;
-      case 'jump':
-        if (this.image !== this.sprites.jump.image) {
-          this.frMax = this.sprites.jump.frMax;
-          this.image = this.sprites.jump.image;
-        }
-        break;
-      case 'slide':
-        if (this.image !== this.sprites.slide.image) {
-          this.frMax = this.sprites.jump.frMax;
-          this.image = this.sprites.jump.image;
-        }
-        break;
-      case 'attack1':
-        if (this.image !== this.sprites.angriff1.image) {
-          this.frame = 0;
-          this.frMax = this.sprites.angriff1.frMax;
-          this.image = this.sprites.angriff1.image;
-        }
-        break;
-      case 'attack2':
-        if (this.image !== this.sprites.angriff2.image) {
-          this.frame = 0;
-          this.frMax = this.sprites.angriff2.frMax;
-          this.image = this.sprites.angriff2.image;
-        }
-        break;
-      case 'magic':
-        if (this.image !== this.sprites.magic.image) {
-          this.frame = 0;
-          this.frMax = this.sprites.magic.frMax;
-          this.image = this.sprites.magic.image;
-        }
-        break;
-      case 'struck':
-        if (this.image !== this.sprites.struck.image) {
-          this.frMax = this.sprites.struck.frMax;
-          this.image = this.sprites.struck.image;
-        }
-        break;
-      case 'death':
-        if (this.image !== this.sprites.death.image) {
-          this.frMax = this.sprites.death.frMax;
-          this.image = this.sprites.death.image;
-        }
-        break;
-    }
-  }
+    // console.log(Math.floor(this.pos.x), Math.floor(this.pos.y))
 
+
+
+
+  }
   // Anzeigen des PCs
   drawPC() {
     ctx.drawImage(
@@ -275,7 +287,7 @@ class Player {
         this.image.width / this.frMax,
         this.image.height,
         canvas.width / 2,
-        this.y,
+        this.pos.y,
         (this.image.width / this.frMax),
         this.image.height
     )
@@ -289,6 +301,14 @@ class Player {
         this.frame++;
       } else {
         this.frame = 0;
+      }
+    }
+    if(this.slideCooldown > 0) this.slideCooldown--;
+    if(this.isSliding){
+      this.slideDuration--;
+      if(this.slideDuration <= 0){
+        this.isSliding = false;
+        this.invulnerable = false;
       }
     }
   }
